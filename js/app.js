e1b98615de5f8ca4e716698008871421b8c4d58f
Sascha Mick <smick@ap.aaronprojects.de>
2016-05-29 18:18:38 +0200

    Ausklappmenü in Mobile Navigation

--- js/app.js
+++ js/app.js
@@ -1,46 +1,66 @@
-var anzahl_bilder = 0;
-var anzahl_container = $('.parallax').length;
-var bildbreite = $(this).width();
-var groesse = "_small";
-var bild;
-var last;
+function parallaxEffekt() {
+    var anzahl_bilder = 0;
+    var bildbreite = $(this).width();
+    var groesse = "_small";
+    var bild;
+    var last;
 
 // Zähle die Anzahl der Dateien im Ordner /bg
 // Die Anzahl wird durch 3 geteilt
 // da jeweils 3 Versionen jedes Bildes existieren
-var getData = $.get('img/count.php', function (data) {
-    anzahl_bilder = data;
-});
+    var getData = $.get('img/count.php', function (data) {
+        anzahl_bilder = data;
+    });
 
 // Setze Größe der Bilder fest die geladen werden sollen
-if (bildbreite > 992) {
-    groesse = "_large";
-} else if (bildbreite > 600) {
-    groesse = "_medium";
-}
+    if (bildbreite > 992) {
+        groesse = "_large";
+    } else if (bildbreite > 600) {
+        groesse = "_medium";
+    }
 
 // Warte bis get Request ein Ergebnis geliefert hat
-$.when(getData).done(function () {
-    // Befülle alle Elemente mit Klassenname .parallax mit den Bildern
-    $('.parallax').each(function (index) {
-        if (index === 0) {
-            bild = 1;
-        } else {
-            last = bild;
-            // Sicherstellen, dass Bilder nicht zweimal nacheinander auftauchen
-            while (last === bild) {
-                bild = parseInt(Math.random() * (anzahl_bilder - 2) + 2);
+    $.when(getData).done(function () {
+        // Befülle alle Elemente mit Klassenname .parallax mit den Bildern
+        $('.parallax').each(function (index) {
+            if (index === 0) {
+                bild = 1;
+            } else {
+                last = bild;
+                // Sicherstellen, dass Bilder nicht zweimal nacheinander auftauchen
+                while (last === bild) {
+                    bild = parseInt(Math.random() * (anzahl_bilder - 2) + 2);
+                }
             }
-        }
 
-        console.log(bild);
+            $(this).html('<img src="img/bg/'
+                    + bild
+                    + groesse
+                    + '.jpg" alt="Hintergrundbild für Parallax Effekt">)');
+        });
 
-        $(this).html('<img src="img/bg/'
-                + bild
-                + groesse
-                + '.jpg" alt="Hintergrundbild für Parallax Effekt">)');
+        // Parallax Effekt aus Materialize Bibliothek
+        $('.parallax').parallax();
+    });
+}
+
+function navActivate() {
+    $('.nav-activator').click(function(){
+        var ulItem = $(this).parent().children('ul');
+        var openLikeHell = ulItem.hasClass('active');
+        
+        // Schließe alle Untermenüs
+        $('.nav-activator').each(function(){
+            $(this).parent().children('ul').removeClass('active');
+        });
+        
+        // Und öffne dann das ausgewählte Menü sofern es nicht schon offen ist
+        if (!openLikeHell) {
+            ulItem.addClass('active');
+        } 
     });
     
-    // Parallax Effekt aus Materialize Bibliothek
-    $('.parallax').parallax();
-});
+}
+
+parallaxEffekt();
+navActivate();
--- js/materialize_init.js
+++ js/materialize_init.js
@@ -1,3 +1,5 @@
 $(document).ready(function () {
     $('.button-collapse').sideNav();
+    $(".dropdown-button").dropdown({beloworigin: false, constrain_width: false});
+    $('input#input_text, textarea#textarea1').characterCounter();
 });
