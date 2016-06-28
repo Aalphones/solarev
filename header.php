3afa227bc4aa56e1dc0e8ffd263dfd1136c5c649
Sascha Mick <Sascha Mick>
2016-06-28 20:13:22 +0200

    Unterseiten hinzufügen

--- header.php
+++ header.php
@@ -11,85 +11,19 @@
     <body>
         <nav>
             <div class="nav-wrapper container">
-                <a href="/home" class="brand-logo">
-                    <img id="logo" src="img/logo.png">
+                <a href="home" class="brand-logo">
+                    <h2>
+                        Sonnenenergie<br>
+                        Neckar-Alb e.V.
+                    </h2>
                 </a>
                 <ul class="right hide-on-med-and-down">
-                    <li><a href="/home">Startseite</a></li>
-                    <li><a href="/aktuelles">Aktuelles</a></li>
-                    <li>
-                        <a class="dropdown-button" data-activates="dropdown1">Der Verein<i class="fa fa-caret-down right" aria-hidden="true"></i></a>
-                        <ul id="dropdown1" class="dropdown-content">
-                            <li><a href="about.html">Über Uns</a></li>
-                            <li><a href="text.html">Die Satzung</a></li>
-                            <li><a href="text.html">Der Vorstand</a></li>
-                            <li class="divider"></li>
-                            <li><a href="text.html">Unsere Mitgliedbeiträge</a></li>
-                            <li><a href="chronik.html">Vereinschronik</a></li>
-                            <li><a href="text.html">Energiekooperativ</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a class="dropdown-button" data-activates="dropdown2">Energieszenario 2030<i class="fa fa-caret-down right" aria-hidden="true"></i></a>
-                        <ul id="dropdown2" class="dropdown-content">
-                            <li><a href="text.html">Das Energieszenario 2030</a></li>
-                            <li class="divider"></li>
-                            <li><a href="text.html">Energieszenario BW</a></li>
-                            <li><a href="text.html">EE-Strom 2009</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a class="dropdown-button" data-activates="dropdown3">Solar-Regionalliga<i class="fa fa-caret-down right" aria-hidden="true"></i></a>
-                        <ul id="dropdown3" class="dropdown-content">
-                            <li><a href="text.html">Die Solar-Regionalliga</a></li>
-                            <li class="divider"></li>
-                            <li><a href="text.html">Solar-Regionalliga 2008/2009</a></li>
-                            <li><a href="text.html">Solar-Regionalliga 2009/2010</a></li>
-                            <li><a href="text.html">Solar-Regionalliga 2010/2011</a></li>
-                        </ul>
-                    </li>
+                    <li><a href="home">Startseite</a></li>
+                    <li><a href="aktuelles">Aktuelles</a></li>
+                    <li><a href="verein">Der Verein</a></li>
+                    <li><a href="energieszenario">Energieszenario 2030</a></li>
+                    <li><a href="regionalliga">Solar-Regionalliga</a></li>
                 </ul>
-
-                <ul id="nav-mobile" class="side-nav">
-                    <li class="header">
-                    <h2>
-                        Sonnenenergie Neckar-Alb e.V.    
-                    </h2>
-                    </li>
-                    <li><a href="/home">Startseite</a></li>
-                    <li><a href="/aktuelles">Aktuelles</a></li>
-                    <li>
-                        <a class="nav-activator">Der Verein<i class="fa fa-caret-down right" aria-hidden="true"></i></a>
-                        <ul>
-                            <li><a href="about.html">Über Uns</a></li>
-                            <li><a href="text.html">Die Satzung</a></li>
-                            <li><a href="text.html">Der Vorstand</a></li>
-                            <li><a href="text.html">Unsere Mitgliedbeiträge</a></li>
-                            <li><a href="chronik.html">Vereinschronik</a></li>
-                            <li><a href="text.html">Energiekooperativ</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a class="nav-activator">Energieszenario 2030<i class="fa fa-caret-down right" aria-hidden="true"></i></a>
-                        <ul>
-                            <li><a href="text.html">Das Energieszenario 2030</a></li>
-                            <li><a href="text.html">Energieszenario BW</a></li>
-                            <li><a href="text.html">EE-Strom 2009</a></li>
-                        </ul>
-                    </li>
-                    <li>
-                        <a class="nav-activator">Solar-Regionalliga<i class="fa fa-caret-down right" aria-hidden="true"></i></a>
-                        <ul>
-                            <li><a href="text.html">Die Solar-Regionalliga</a></li>
-                            <li><a href="text.html">Solar-Regionalliga 2008/2009</a></li>
-                            <li><a href="text.html">Solar-Regionalliga 2009/2010</a></li>
-                            <li><a href="text.html">Solar-Regionalliga 2010/2011</a></li>
-                        </ul>
-                    </li>
-                </ul>
-                <a href="" data-activates="nav-mobile" class="button-collapse">
-                    <i class="fa fa-bars"></i>
-                </a>
             </div>
         </nav>
         <main>
--- index.php
+++ index.php
@@ -4,15 +4,17 @@ include dirname(__FILE__) . '/AltoRouter.php';
 
 // Neue Instanz von AltoRouter erstellen und Basepath setzen
 $router = new AltoRouter();
-$router->setBasePath('');
+$router->setBasePath('/solarev');
 
 // Definition der Routen
 $router->map('GET','/', 'pages/home.html', 'home');
 $router->map('GET','/home', 'pages/home.html', 'home-home');
 $router->map('GET','/impressum', 'pages/impressum.html', 'impressum');
 $router->map('GET','/kontakt', 'pages/kontakt.html', 'kontakt');
-$router->map('GET','/aktuelles', 'pages/news/index.html', 'news');
-$router->map('GET','/aktuelles/[*:news_id]', 'pages/news/index.html', 'news-detail');
+$router->map('GET','/aktuelles', 'pages/aktuelles.html', 'news');
+$router->map('GET','/energieszenario', 'pages/energieszenario.html', 'energieszenario');
+$router->map('GET','/verein', 'pages/verein.html', 'verein');
+$router->map('GET','/regionalliga', 'pages/regionalliga.html', 'regionalliga');
 
 // Header einbinden
 include 'header.php';
